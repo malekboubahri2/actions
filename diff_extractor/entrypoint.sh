755b873d93bdb7ec1b5bfa05c579b1b170187300
Malek Boubahri <malek.boubahri@st.com>
2025-04-15 17:47:40 +0100

    Update

--- diff_extractor/entrypoint.sh
+++ diff_extractor/entrypoint.sh
@@ -13,5 +13,15 @@ last_commit_hash=$(git rev-parse HEAD)
 # Get the hash of the second-to-last commit
 previous_commit_hash=$(git rev-parse HEAD^1)
 
+# Get the diff between the last commit and the previous commit and save it to a file
+diff_file="diff_output.txt"
+git diff $previous_commit_hash $last_commit_hash > "$diff_file"
+
+# Read the file content
+diff_result=$(cat "$diff_file")
+
+# Escape the diff result for safe output
+escaped_diff_result=$(echo "$diff_result" | sed 's/%/%25/g' | sed 's/\n/%0A/g' | sed 's/\r/%0D/g')
+
 # Set the output
-echo "::set-output name=diff-result::$(git diff $previous_commit_hash $last_commit_hash)"
+echo "::set-output name=diff-result::$escaped_diff_result"
