1b4deee229188dc59788c0114a382b679b87b921
Malek Boubahri <malek.boubahri@st.com>
2025-04-15 16:29:07 +0100

    Update

--- diff_extractor/entrypoint.sh
+++ diff_extractor/entrypoint.sh
@@ -1,7 +1,10 @@
-#!/bin/sh -l
+#!/bin/bash
 
-# Navigate to the repository path
-cd "$1" || exit 1
+# Ensure the script is executed in a Git repository
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    echo "Error: This script must be run inside a Git repository."
+    exit 1
+fi
 
 # Get the hash of the last commit
 last_commit_hash=$(git rev-parse HEAD)
@@ -9,8 +12,5 @@ last_commit_hash=$(git rev-parse HEAD)
 # Get the hash of the second-to-last commit
 previous_commit_hash=$(git rev-parse HEAD^)
 
-# Get the diff between the last commit and the previous commit
-diff_result=$(git diff $previous_commit_hash $last_commit_hash)
-
-# Set the output
-echo "::set-output name=diff-result::$diff_result"
+# Display the diff between the last commit and the previous commit
+git diff $previous_commit_hash $last_commit_hash
