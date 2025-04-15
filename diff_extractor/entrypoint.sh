29dec7cd38d06a32405022cf0f46ccff5d5d50a9
Malek Boubahri <malek.boubahri@st.com>
2025-04-15 16:31:58 +0100

    Update

--- diff_extractor/entrypoint.sh
+++ diff_extractor/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ls
 # Ensure the script is executed in a Git repository
 if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
     echo "Error: This script must be run inside a Git repository."
